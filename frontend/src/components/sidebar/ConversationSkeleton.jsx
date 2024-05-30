919d2ca73a7cbe9ece9994d0dc3caf25a88cc750
shudipta-majumder <shudiptamazumdar@gmail.com>
2024-05-30 17:34:41 +0600

    button

--- frontend/src/components/sidebar/ConversationSkeleton.jsx
+++ frontend/src/components/sidebar/ConversationSkeleton.jsx
@@ -11,14 +11,10 @@ const ConversationSkeleton = () => {
           </div>
         </div>
       ) : (
-        <div className="flex gap-16 items-center rounded p-2 py-1 bg-gray-700 animate-pulse">
+        <div className="flex gap-20 items-center rounded p-2 py-1 bg-gray-700 animate-pulse">
           <div className="avatar">
             <div className="w-12 h-12 rounded-full bg-gray-600"></div>
           </div>
-          <div className="avatar">
-            <div className="w-12 h-12 rounded-full bg-gray-600"></div>
-          </div>
-
           <div className="flex gap-16 flex-col flex-1">
             <div className="flex gap-3 justify-between">
               <div className="w-24 h-4 bg-gray-600 rounded"></div>
