b47ef26a49b067a6e58ae291b64a5250ea889e4d
shudipta-majumder <shudiptamazumdar@gmail.com>
2024-05-30 17:27:22 +0600

    button

--- frontend/src/components/sidebar/ConversationSkeleton.jsx
+++ frontend/src/components/sidebar/ConversationSkeleton.jsx
@@ -1,19 +1,29 @@
+import useMediaQuery from "../../hooks/useMediaquery";
+
 const ConversationSkeleton = () => {
-    return (
-      <div className="flex gap-16 items-center rounded p-2 py-1 bg-gray-700 animate-pulse">
+  const underSM = useMediaQuery("(max-width: 640px)");
+  return (
+    <div className="flex gap-16 items-center rounded p-2 py-1 bg-gray-700 animate-pulse">
+      {underSM ? (
         <div className="avatar">
           <div className="w-12 h-12 rounded-full bg-gray-600"></div>
         </div>
-  
-        <div className="flex gap-16 flex-col flex-1">
-          <div className="flex gap-3 justify-between">
-            <div className="w-24 h-4 bg-gray-600 rounded"></div>
-            <div className="w-6 h-6 bg-gray-600 rounded-full"></div>
+      ) : (
+        <div>
+          <div className="avatar">
+            <div className="w-12 h-12 rounded-full bg-gray-600"></div>
+          </div>
+
+          <div className="flex gap-16 flex-col flex-1">
+            <div className="flex gap-3 justify-between">
+              <div className="w-24 h-4 bg-gray-600 rounded"></div>
+              <div className="w-6 h-6 bg-gray-600 rounded-full"></div>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  };
-  
-  export default ConversationSkeleton;
-  
+      )}
+    </div>
+  );
+};
+
+export default ConversationSkeleton;
