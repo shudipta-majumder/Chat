2fe883b0d0e9324eae87e04487fb28f31ed1ce58
shudipta-majumder <shudiptamazumdar@gmail.com>
2024-05-30 18:41:14 +0600

    button

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -11,29 +11,40 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 
   return (
     <>
-      <div
-        className={`flex gap-4 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-				${isSelected ? "bg-sky-500" : ""}
+      {underSM ? (
+        <div
+          className={`flex gap-4 items-center hover:bg-purple-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-purple-500" : ""}
 			`}
-        onClick={() => setSelectedConversation(conversation)}
-      >
-        <div className={`avatar ${isOnline ? "online" : ""}`}>
-          <div className="w-12 rounded-full">
-            <img src={conversation.profilePic} alt="user avatar" />
-          </div>
+          onClick={() => setSelectedConversation(conversation)}
+        >
+          <div className={`avatar ${isOnline ? "online" : ""}`}>
+            <div className="w-12 rounded-full">
+              <img src={conversation.profilePic} alt="user avatar" />
+            </div>
+          </div>{" "}
         </div>
+      ) : (
+        <div
+          className={`flex gap-4 items-center hover:bg-purple-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-purple-500" : ""}
+			`}
+          onClick={() => setSelectedConversation(conversation)}
+        >
+          <div className={`avatar ${isOnline ? "online" : ""}`}>
+            <div className="w-12 rounded-full">
+              <img src={conversation.profilePic} alt="user avatar" />
+            </div>
+          </div>
 
-        {underSM ? (
-          ""
-        ) : (
           <div className="flex flex-col flex-1">
             <div className="flex gap-3 justify-between">
               <p className="font-bold text-gray-200">{conversation.fullName}</p>
               <span className="text-xl">{emoji}</span>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
 
       {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
