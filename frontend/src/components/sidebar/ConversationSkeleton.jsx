b4b9a72ac4defca31a187dcb12da1e9627e8ab09
shudipta-majumder <shudiptamazumdar@gmail.com>
2024-05-30 17:37:00 +0600

    button

--- frontend/src/components/sidebar/ConversationSkeleton.jsx
+++ frontend/src/components/sidebar/ConversationSkeleton.jsx
@@ -16,7 +16,7 @@ const ConversationSkeleton = () => {
             <div className="w-12 h-12 rounded-full bg-gray-600"></div>
           </div>
           <div className="flex gap-16 flex-col flex-1">
-            <div className="flex gap-3 justify-between">
+            <div className="flex gap-10 justify-between">
               <div className="w-24 h-4 bg-gray-600 rounded"></div>
               <div className="w-6 h-6 bg-gray-600 rounded-full"></div>
             </div>
